c6c9523cbc297ed10b7decca513e13948cf0319e
Karl Mottus <karlmottus@Karls-MBP.lan>
2019-11-12 21:51:00 +0200

    Added Item - Working solution

---
+++ NoteKeeper/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NoteKeeper.Models
+{
+    public class Item
+    {
+        public string Text { get; set;}
+        public string Id { get; set; }
+        public string Description { get; set; }
+    }
+}
